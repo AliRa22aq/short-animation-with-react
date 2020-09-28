10326cdfee5288bf3457f1cc4cc5274a4e16f0da
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-09-28 12:17:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 
 import React from "react";
-import useWebAnimations, { slideInLeft} from "@wellyshen/use-web-animations";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import Footer from "./components/Footer";
@@ -8,8 +7,6 @@ import Body2 from "./components/Body2";
 import Body3 from "./components/Body3";
 
 function App() {
-  const { ref } = useWebAnimations({ ...slideInLeft  });
-
   return (
     <div className="container" >
       <Header />
