560a89feb6a2b532cbd5c394f6c910ebad9e04dd
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-09-30 17:00:09 +0500

    Update Body2.js

--- src/components/Body2.js
+++ src/components/Body2.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
+
 function Body2() {
+
+
     return (
-        <div style={{ backgroundColor: "offwhite", height: "100vh"}}>
-            I am Body 2
+        <div id = 'body2'>
+            Hello I am body 2
         </div>
     )
 }
