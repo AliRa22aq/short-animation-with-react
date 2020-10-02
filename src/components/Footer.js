819f0bc39e0acb5df41942f94a18d803362978bb
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-10-03 03:31:37 +0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import '../App.css';
 function Footer() {
 
     return (
-        <div id="footer" style={{ backgroundColor: "Yellow", height: "40vh", display: "flex", alignItems: "center"}}>
+        <div id="footer" style={{ color: "whitesmoke", backgroundColor: "black", height: "30vh", display: "flex", alignItems: "center"}}>
             I am Footer
 
             <a href="#header"> Top </a>
