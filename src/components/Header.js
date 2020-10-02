2e7b45ab87d42bccfd5e863b55cf90939856b20c
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-10-02 16:31:01 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,39 @@
-import React from 'react'
+import { Button, Grid } from '@material-ui/core';
+import React from 'react';
+import '../App.css';
+import 'react-sticky-header/styles.css';
+import StickyHeader from 'react-sticky-header';
 
 
 function Header() {
+
     return (
-        <div style={{ backgroundColor: "grey", height: "10vh", display: "flex", alignItems: "center"}}>
-            <a href="#body2"> Body2 7</a>
-        </div>
+        <div id= "header">
+        <StickyHeader
+        // This is the sticky part of the header.
+                header={
+
+                    <div  style={{height: "15vh", display: "flex", alignItems: "center"}}>
+
+                    <Grid container style = {{color: "white"}} > 
+                    <Grid item xs={2} style = {{alignSelf:"center", paddingLeft:"30px", fontWeight:"2.5rem", alignContent: "center",  fontSize: "1.25rem"}}> 
+                    <a href="#header"> BOOKLAND </a> 
+                    </Grid>
+                    <Grid item xs={6}> </Grid>
+                    <Grid item xs={1}> <Button > <a href="#header"> HOME </a> </Button> </Grid> 
+                    <Grid item xs={1}> <Button > <a href="#body2"> Section1 </a> </Button> </Grid> 
+                    <Grid item xs={1}> <Button > <a href="#body3"> Section2 </a> </Button> </Grid> 
+                    <Grid item xs={1}> <Button > <a href="#body4"> Section3 </a> </Button> </Grid> 
+
+                    
+                    </Grid>
+                    </div>
+                }/>
+                </div>
+        
+
     )
 }
 
 export default Header
+
