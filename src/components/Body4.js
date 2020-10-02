995875578f0a447d9d26bfd4875b26e8fbe59e14
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-10-02 16:30:54 +0500

    Create Body4.js

---
+++ src/components/Body4.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import x from '../pictures/file.svg'
+
+function Body4() {
+    return (
+        <div id= 'body4'>
+            I am Body 4
+        </div>
+    )
+}
+
+export default Body4
