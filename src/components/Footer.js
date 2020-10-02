d513450d5c4e9713213e3c7651ddebb6b0a2b5a6
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-10-02 16:30:58 +0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import '../App.css';
 
 function Footer() {
+
     return (
-        <div style={{ backgroundColor: "Yellow", height: "40vh", display: "flex", alignItems: "center"}}>
+        <div id="footer" style={{ backgroundColor: "Yellow", height: "40vh", display: "flex", alignItems: "center"}}>
             I am Footer
+
+            <a href="#header"> Top </a>
         </div>
+    
     )
 }
 
