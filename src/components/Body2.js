0ae0e38bdac3083c1120860f4cffa7fdf8e36e36
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-10-02 16:31:19 +0500

    Update Body2.js

--- src/components/Body2.js
+++ src/components/Body2.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React from 'react';
+import ReactLogo from '../pictures/file.svg';
+
 
 
 function Body2() {
 
-
     return (
         <div id = 'body2'>
+            
             Hello I am body 2
+
         </div>
     )
 }
