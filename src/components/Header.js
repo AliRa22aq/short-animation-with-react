e6c51262c22248089c7040fc9bf6a441be94d32f
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-10-02 20:21:20 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,14 +17,14 @@ function Header() {
 
                     <Grid container style = {{color: "white"}} > 
                     <Grid item xs={2} style = {{alignSelf:"center", paddingLeft:"30px", fontWeight:"2.5rem", alignContent: "center",  fontSize: "1.25rem"}}> 
-                    <a href="#header"> BOOKLAND </a> 
+                    <a href="#header"> BOOKS.PK </a> 
                     </Grid>
-                    <Grid item xs={6}> </Grid>
-                    <Grid item xs={1}> <Button > <a href="#header"> HOME </a> </Button> </Grid> 
-                    <Grid item xs={1}> <Button > <a href="#body2"> Section1 </a> </Button> </Grid> 
-                    <Grid item xs={1}> <Button > <a href="#body3"> Section2 </a> </Button> </Grid> 
-                    <Grid item xs={1}> <Button > <a href="#body4"> CONTACT </a> </Button> </Grid> 
-
+                    <Grid item xs={5}> </Grid>
+                    <Grid item xs={1} > <Button > <a href="#header"> HOME </a> </Button> </Grid> 
+                    <Grid item xs={1} > <Button > <a href="#body2"> ABOUT </a> </Button> </Grid> 
+                    <Grid item xs={1} > <Button > <a href="#body3"> OFFERS </a> </Button> </Grid> 
+                    <Grid item xs={1} > <Button > <a href="#body3"> DICOVER </a> </Button> </Grid> 
+                    <Grid item xs={1} > <Button > <a href="#body4"> CONTACT </a> </Button> </Grid> 
                     
                     </Grid>
                     </div>
