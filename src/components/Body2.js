b42daca9c2cacda61aa9cda8ab32d2d15c32631d
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-10-03 03:31:22 +0500

    Update Body2.js

--- src/components/Body2.js
+++ src/components/Body2.js
@@ -1,33 +1,65 @@
-import { Grid } from '@material-ui/core';
+import { Button, Grid } from '@material-ui/core';
 import React from 'react';
-
-
+import useWebAnimations, { pulse} from "@wellyshen/use-web-animations";
 
 function Body2() {
+
+    var { keyframes, timing } = pulse;
+    const { ref: b2 } = useWebAnimations({
+      keyframes,
+      timing: {
+        ...timing,
+        delay: 3000, // Delay 1s
+        iterations: Infinity,
+        direction: "alternate", // Run the animation forwards and then backwards
+        easing: "ease-in-out", // Use a fancy timing function
+      },
+    });
 
     return (
         <div id = 'body2'>
             <Grid container> 
+            
 
-            <Grid item xs={8} style = {{ fontWeight: "2rem", fontFamily: "arial", alignContent: "center", fontSize: "2rem"}}> 
-            <div style={{padding: "200px 0px 0px 100px"}}> 
-                WE ARE PAKISTAN'S BIGGEST BOOK DONATION AGENCY
+            <Grid item xs={8} style = {{ color:"white", fontWeight: "2rem", fontFamily: "arial", alignContent: "center", fontSize: "2rem"}}> 
+            
+            <div style={{padding: "150px 0px 0px 100px"}}> 
+              WE ARE PAKISTAN'S BIGGEST BOOK DONATION AGENCY 
             </div>
-            <div style={{padding: "10px 0px 0px 100px"}}> 
-                WE CONNECT BOOKS TO PEOPLE SO THEY SPEND LESS TIME ON YOUR SHELVES
+            
+            
+            <div style={{padding: "20px 0px 0px 100px"}}> 
+               WE CONNECT BOOKS TO PEOPLE SO THEY SPEND LESS TIME ON YOUR SHELVES
             </div>
-            <div style={{padding: "10px 0px 0px 100px"}}> 
-                OUR MISSION IS TO MAKE BOOKS AVAILABLE IN EVERY HOUSE OF PAKISTAN
+            
+            
+            <div style={{padding: "20px 0px 0px 100px"}}> 
+             OUR MISSION IS TO MAKE BOOKS AVAILABLE IN EVERY HOUSE OF PAKISTAN 
+            </div> 
+            <div style={{ color:"red", padding: "100px 0px 0px 570px"}}> 
+                <Button style={{ border:"1px", color:"red", fontSize:"1.25rem"}}> LEARN MORE </Button>
             </div>
+           
             </Grid>
             <Grid item xs={4}> 
-            <div style={{padding: "200px 0px 0px 0px"}}> 
+            <div  style={{position: "absolute", padding: "190px 0px 0px 0px"}}> 
                 <img 
                 src='https://images.vexels.com/media/users/3/144860/isolated/preview/5128b7ff4b40f49cd6c0ce8698b9a19a-boy-reading-books-illustration-by-vexels.png' 
                 alt='img'
-                height="400px"
-                width="400px" />
+                height="350px"
+                width="350px" />
             </div>
+
+            <div ref={b2} style={{position: "absolute", padding: "175px 0px 0px 210px"}}> 
+                <img 
+                src='https://freesvg.org/img/1527134288.png' 
+                alt='img'
+                height="150px"
+                width="100px" />
+            </div>
+            
+
+
             </Grid>
 
             </Grid>
