eb4084342799cf955d184fff54aa438bbf0f9839
Ali Razzaq <59477206+AliRa22aq@users.noreply.github.com>
2020-10-03 19:40:57 +0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,81 @@
 import React from 'react'
 import '../App.css';
+import { Grid } from '@material-ui/core';
+import useWebAnimations, { shakeY} from "@wellyshen/use-web-animations";
+
+
 
 function Footer() {
 
-    return (
-        <div id="footer" style={{ color: "whitesmoke", backgroundColor: "black", height: "30vh", display: "flex", alignItems: "center"}}>
-            I am Footer
+    var { keyframes, timing } = shakeY;
+    const { ref: b1 } = useWebAnimations({
+      keyframes,
+      timing: {
+        ...timing,
+        iterations: Infinity,
+        direction: "alternate", // Run the animation forwards and then backwards
+        easing: "ease-in-out", // Use a fancy timing function
+      },
+    });
 
-            <a href="#header"> Top </a>
+
+
+    return (
+        <div  style={{ color: "whitesmoke", backgroundColor: "black", height: "30vh", display: "flex", alignItems: "center"}}>
+            <Grid container>
+
+                <Grid item xs={3}> </Grid>
+                <Grid item xs={3}> 
+                <div style={{display: "flex", fontSize: "1.75rem", paddingTop: "5px"}}>
+                    <text> Creater by: Ali Razzaq </text>
+                    
+                </div>
+                </Grid>
+
+                <Grid item xs={1}>
+                    <div style={{display: "flex", alignItems: "center"}}>  
+                <a href="https://www.facebook.com/AliRa22aQ/"> 
+                <img src='https://upload.wikimedia.org/wikipedia/en/thumb/8/8c/Facebook_Home_logo_old.svg/1200px-Facebook_Home_logo_old.svg.png'
+                alt = "top"
+                height="50px"
+                width= "50px" 
+                />
+                </a> </div> </Grid>
+
+                <Grid item xs={1}>
+                    <div style={{display: "flex", alignItems: "center"}}>  
+                <a href="https://www.linkedin.com/in/alirazzaquet" name='LinkedIn'> 
+                <img src='https://media2.estateweb.com/contentimage.ewdgx?6562E55DC7948D800704F05636242F42C3C44B38E04198DB14B4BAEC4C6DE476FDEC8C3D7DC6E167400978A43F955973'
+                alt = "top"
+                height="50px"
+                width= "50px" 
+                />
+                </a> </div> </Grid>
+
+                <Grid item xs={1}>
+                    <div style={{display: "flex", alignItems: "center"}}>  
+                <a href="https://github.com/AliRa22aq"> 
+                <img src='https://www.flaticon.com/svg/static/icons/svg/2111/2111374.svg'
+                alt = "top"
+                height="50px"
+                width= "50px" 
+                />
+                </a> </div> </Grid>
+                <Grid item xs={2}></Grid>
+
+                <Grid item xs={1}>
+                    <div ref={b1} style={{display: "flex", alignItems: "center"}}>  
+                <a href="#header"> 
+                <img src='https://upload.wikimedia.org/wikipedia/commons/8/85/Arrow_top_svg.svg'
+                alt = "top"
+                height="20px"
+                width= "20px" 
+                />
+                </a> </div> </Grid>
+
+            </Grid>
+
+           
         </div>
     
     )
